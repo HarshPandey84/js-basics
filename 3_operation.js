32aca2011896df59cb62822ff7759df3e005c291
HarshPandey84 <146015613+HarshPandey84@users.noreply.github.com>
2024-09-15 00:16:04 +0530

    Update 3_operation.js

--- 3_operation.js
+++ 3_operation.js
@@ -13,3 +13,22 @@ console.log(1+"2")
 console.log("1"+2)
 console.log("1"+2+2)
 console.log(2+2+"1")
+let val=3
+let newval=-val
+console.log(newval)
+
+// console.log(2+2);  -,*,/,**,%
+
+let x="hello"
+let y=" world"
+let z=x+y
+console.log(z)
+
+console.log(1+"2") //12
+console.log("1"+2)  //12
+console.log("1"+2+2) //122
+console.log(2+2+"1") //41
+console.log(true)  //true  ,+true->1 not advised
+let a=1
+a++
+console.log(a) //2
