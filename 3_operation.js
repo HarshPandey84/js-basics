03a50f15599e512af4f7c404205a988495525b13
HarshPandey84 <146015613+HarshPandey84@users.noreply.github.com>
2024-09-15 00:18:53 +0530

    Update 3_operation.js

--- 3_operation.js
+++ 3_operation.js
@@ -1,23 +1,8 @@
 let val=3
 let newval=-val
-console.log(newval)
+console.log(newval) // will give -3
 
-// console.log(2+2);  -,*,/,**,%
-
-let x="hello"
-let y=" world"
-let z=x+y
-console.log(z)
-
-console.log(1+"2")
-console.log("1"+2)
-console.log("1"+2+2)
-console.log(2+2+"1")
-let val=3
-let newval=-val
-console.log(newval)
-
-// console.log(2+2);  -,*,/,**,%
+// console.log(2+2);  -,*,/,**,%   will give output acc to operation
 
 let x="hello"
 let y=" world"
@@ -31,4 +16,4 @@ console.log(2+2+"1") //41
 console.log(true)  //true  ,+true->1 not advised
 let a=1
 a++
-console.log(a) //2
+console.log(a)  //gives two
